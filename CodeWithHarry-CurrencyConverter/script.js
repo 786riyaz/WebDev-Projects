35fc70906ea338d0269419bea9436c849ab7bda9
Riyaz <riyaz.pathan@streebo.com>
2023-09-03 21:28:26 +0530

    Added Changes basic structure for project

---
+++ CodeWithHarry-CurrencyConverter/script.js
@@ -0,0 +1,14 @@
+// import currencyapi from '@everapi/currencyapi-js'
+
+// const client = new currencyapi('YOUR-API-KEY')
+// await client.latest()
+
+console.log("Application Started");
+
+// url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_aXn4PmHLh2XrEMl0jCx4b8EkzkCtohocyZthujuQ";
+
+// base_currency=INR
+debugger;
+const tableBody = document.getElementById("table");
+tableBody.innerHTML = `<tr><td>Data</td><td>Data</td><td>Data</td></tr>`;
+console.log("Done")
